661bff66be0be66af88e5eb282dcc3cce41a259a
TichkovAydar <aydar15@gmail.com>
2020-01-05 02:31:46 +0300

    Calculator V0.1

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+alert("Check it out, Nizam!")
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
